1d46035642ea41a04f4cb78c0aad9b979f1c5b7f
AidanLDev <dev@aidanlowson.com>
2022-08-26 07:36:41 +0100

    Adding integrations and a 404 page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // site: TODO: Add site URL here e.g. https://stargazers.club
+  integrations: [sitemap(), react()]
+});
